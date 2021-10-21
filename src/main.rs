1802d076802a0c1b2d96a40aa83872a81e1be29d
Pawel Lipinski <lipinskijob@gmail.com>
2021-10-21 17:05:35 +0200

    Create and parametrized progress function

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ use std::{thread::sleep, time::Duration};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
+fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
+    let mut i = 1;
+    for n in v.iter() {
+        println!("{}{}", CLEAR, "*".repeat(i));
+        i += 1;
+        f(n);
+    }
+}
+
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
 
 fn main() {
     let v = vec![1, 2, 3];
-    let mut i = 1;
-    for n in v.iter() {
-        println!("{}{}", CLEAR, "*".repeat(i));
-        i += 1;
-        expensive_calculation(n);
-    }
+    progress(v, expensive_calculation);
 }
